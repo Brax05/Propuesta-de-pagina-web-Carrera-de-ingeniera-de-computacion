12682246b6b7ce091b8ada3b5965f33c8f8b07ad
Brandon muñoz <101955710+Brax05@users.noreply.github.com>
2025-11-11 22:56:54 -0300

    Enhance vite.config.ts for ESM support
    Added ESM support by defining __dirname and importing fileURLToPath.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url' // <-- AÑADE ESTA LÍNEA
+
+// AÑADE ESTAS DOS LÍNEAS PARA DEFINIR __dirname EN ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // ESTO AHORA FUNCIONARÁ CREO 
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
