50f70b0b2ba20b7289008b308498264653572167
Brandon muñoz <101955710+Brax05@users.noreply.github.com>
2025-11-11 22:58:33 -0300

    Update vite.config.ts to include fileURLToPath
    Added import for fileURLToPath and defined __dirname.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+// vite.config.ts
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url' // <-- AÑADE ESTA LÍNEA
+import { fileURLToPath } from 'url'
 
-// AÑADE ESTAS DOS LÍNEAS PARA DEFINIR __dirname EN ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,7 +13,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // ESTO AHORA FUNCIONARÁ CREO 
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
